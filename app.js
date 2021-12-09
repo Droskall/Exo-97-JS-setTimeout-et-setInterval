40d462b247f2fffc83deb857ca434a7d2289ed76
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-12-09 13:12:08 +0100

    finish

---
+++ app.js
@@ -0,0 +1,21 @@
+
+document.getElementById("timeout").addEventListener("click", () => {
+    setTimeout(() => {
+        let div = document.createElement("div");
+        div.style.backgroundColor = "red";
+        div.innerHTML = "Mon timeout vient de creer ce div 2 secondes après le click sur le bouton !";
+        document.getElementById("timeout-div").append(div);
+    }, 2000)
+});
+
+let x = 0;
+
+document.getElementById("interval-start").addEventListener("click", time);
+
+function time() {
+    let interval = setInterval(function () {
+        document.getElementById("interval-div").innerHTML = x;
+        x+=60;
+    }, 2000);
+    document.getElementById("interval-stop").addEventListener("click", () => clearInterval(interval));
+}
